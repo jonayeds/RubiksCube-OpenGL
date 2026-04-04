0bb34e7c661eea64ad54b8b061ff2c40a4a2ae99
jonayeds <jonayeeds@gmail.com>
2026-04-04 17:08:42 +0600

    added gaps in the cubes

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ float scale = 0.5;
 
 static void resize(int width, int height)
 {
-    const float ar = (float) width / (float) height;
+    const float ar = (float)width / (float)height;
 
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
@@ -26,167 +26,174 @@ static void resize(int width, int height)
     glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
 
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
+    glLoadIdentity();
 }
-void Quads(int x, int y, int z)
+void Quads(float x, float y, float z)
 {
     glBegin(GL_QUADS);
-    int max_x=x+1;
-    int max_y=y+1;
-    int max_z=z+1;
-    
+    float max_x = x + 1.0f;
+    float max_y = y + 1.0f;
+    float max_z = z + 1.0f;
 
     // red front
     glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(x,max_y,z);
-    glVertex3f(max_x,max_y,z);
-    glVertex3f(max_x,y,z);
-    glVertex3f(x,y,z);
+    glVertex3f(x, max_y, z);
+    glVertex3f(max_x, max_y, z);
+    glVertex3f(max_x, y, z);
+    glVertex3f(x, y, z);
 
     // yellow left
     glColor3f(1.0f, 1.0f, 0.0f);
-    glVertex3f(x,max_y,max_z);
-    glVertex3f(x,max_y,z);
-    glVertex3f(x,y,z);
-    glVertex3f(x,y,max_z);
+    glVertex3f(x, max_y, max_z);
+    glVertex3f(x, max_y, z);
+    glVertex3f(x, y, z);
+    glVertex3f(x, y, max_z);
 
     // blue back
     glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(max_x,max_y,max_z);
-    glVertex3f(x,max_y,max_z);
-    glVertex3f(x,y,max_z);
-    glVertex3f(max_x,y,max_z);
+    glVertex3f(max_x, max_y, max_z);
+    glVertex3f(x, max_y, max_z);
+    glVertex3f(x, y, max_z);
+    glVertex3f(max_x, y, max_z);
 
     // green right
     glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(max_x,max_y,z);
-    glVertex3f(max_x,max_y,max_z);
-    glVertex3f(max_x,y,max_z);
-    glVertex3f(max_x,y,z);
-
-
+    glVertex3f(max_x, max_y, z);
+    glVertex3f(max_x, max_y, max_z);
+    glVertex3f(max_x, y, max_z);
+    glVertex3f(max_x, y, z);
 
     // magenta bottom
     glColor3f(1.0f, 0.0f, 1.0f);
-    glVertex3f(max_x,y,max_z);
-    glVertex3f(x,y,max_z);
-    glVertex3f(x,y,z);
-    glVertex3f(max_x,y,z);
+    glVertex3f(max_x, y, max_z);
+    glVertex3f(x, y, max_z);
+    glVertex3f(x, y, z);
+    glVertex3f(max_x, y, z);
 
     // cyan top
     glColor3f(0.0f, 1.0f, 1.0f);
-    glVertex3f(max_x,max_y,z);
-    glVertex3f(x,max_y,z);
-    glVertex3f(x,max_y,max_z);
-    glVertex3f(max_x,max_y,max_z);
+    glVertex3f(max_x, max_y, z);
+    glVertex3f(x, max_y, z);
+    glVertex3f(x, max_y, max_z);
+    glVertex3f(max_x, max_y, max_z);
 
     glEnd();
 }
 static void display(void)
 {
     const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
+    const double a = t * 90.0;
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
-
+    glColor3d(1, 0, 0);
 
     glPushMatrix();
-        glTranslated(0, 0, -7);
-        glRotated(degreeX, 1.0, 0.0, 0.0);
-        glRotated(degreeY, 0.0, 1.0, 0.0);
-        glRotated(degreeZ, 0.0, 0.0, 1.0);
-        glScalef(scale, scale, scale);
-        glTranslated(-1.5, -1.5, -1.5);  
-        for (int i=0;i<3;i++){
-            for (int j=0;j<3;j++){
-                for (int k=0;k<3;k++){
-                    Quads(i, j, k);
-                }
+    glTranslated(0, 0, -7);
+    glRotated(degreeX, 1.0, 0.0, 0.0);
+    glRotated(degreeY, 0.0, 1.0, 0.0);
+    glRotated(degreeZ, 0.0, 0.0, 1.0);
+    glScalef(scale, scale, scale);
+    const float gap = 0.1f;
+    const float step = 1.0f + gap;
+    const float span = 3.0f * 1.0f + 2.0f * gap;
+    const float base = -span / 2.0f;
+    glTranslated(base, base, base);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                Quads(i * step, j * step, k * step);
             }
         }
+    }
 
     glPopMatrix();
 
     glutSwapBuffers();
 }
 
-
-
-
 static void key(unsigned char key, int x, int y)
 {
     switch (key)
     {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
+    case 27:
+    case 'q':
+        exit(0);
+        break;
 
-        case 'a':
-            degreeX+=2.5;
-            break;
+    case 'a':
+        degreeX += 2.5;
+        break;
 
-        case 'b':
-            degreeX-=2.5;
-            break;
+    case 'b':
+        degreeX -= 2.5;
+        break;
 
-        case 'c':
-            degreeY+=2.5;
-            break;
+    case 'c':
+        degreeY += 2.5;
+        break;
 
-        case 'd':
-            degreeY-=2.5;
-            break;
+    case 'd':
+        degreeY -= 2.5;
+        break;
 
-        case 'e':
-            degreeZ+=2.5;
-            break;
+    case 'e':
+        degreeZ += 2.5;
+        break;
 
-        case 'f':
-            degreeZ-=2.5;
-            break;
+    case 'f':
+        degreeZ -= 2.5;
+        break;
 
-        case 'm':
-        if (scale<2.0){
-            scale+=0.05;
+    case 'm':
+        if (scale < 2.0)
+        {
+            scale += 0.05;
         }
-            break;
-        case 'n':
-        if (scale>0.1){
-            scale-=0.05;    
+        break;
+    case 'n':
+        if (scale > 0.1)
+        {
+            scale -= 0.05;
         }
-        }
+    }
     glutPostRedisplay();
 }
 
-
-void specialKey(int key, int x, int y) {
+void specialKey(int key, int x, int y)
+{
     const int mods = glutGetModifiers();
     const bool shift = mods == GLUT_ACTIVE_SHIFT;
-    switch (key) {
-        case GLUT_KEY_LEFT:
-            glTranslated(-0.05, 0, 0); 
-            break;
-        case GLUT_KEY_RIGHT:
-            glTranslated(0.05, 0, 0);
-            break;
-        case GLUT_KEY_UP:    
-            if (shift) {
-                glTranslated(0, 0, -0.05);   
-            }
-            else {  
-            glTranslated(0, 0.05, 0);   
-            }
-            break;
-        case GLUT_KEY_DOWN:
-            if (shift) {
-                glTranslated(0, 0, 0.05);   
-            }
-            else {
-                glTranslated(0, -0.05, 0);
-            }
-            break;
+    switch (key)
+    {
+    case GLUT_KEY_LEFT:
+        glTranslated(-0.05, 0, 0);
+        break;
+    case GLUT_KEY_RIGHT:
+        glTranslated(0.05, 0, 0);
+        break;
+    case GLUT_KEY_UP:
+        if (shift)
+        {
+            glTranslated(0, 0, -0.05);
+        }
+        else
+        {
+            glTranslated(0, 0.05, 0);
+        }
+        break;
+    case GLUT_KEY_DOWN:
+        if (shift)
+        {
+            glTranslated(0, 0, 0.05);
+        }
+        else
+        {
+            glTranslated(0, -0.05, 0);
+        }
+        break;
     }
     glutPostRedisplay();
 }
@@ -200,22 +207,22 @@ int main(int argc, char *argv[])
 {
 
     cout << "------------- Keyboard Input -------------" << endl;
-    cout << "a: Rotate X-axis +2.5 degree" << endl; 
-    cout << "b: Rotate X-axis -2.5 degree" << endl; 
+    cout << "a: Rotate X-axis +2.5 degree" << endl;
+    cout << "b: Rotate X-axis -2.5 degree" << endl;
     cout << "c: Rotate Y-axis +2.5 degree" << endl;
-    cout << "d: Rotate Y-axis -2.5 degree" << endl;     
+    cout << "d: Rotate Y-axis -2.5 degree" << endl;
     cout << "e: Rotate Z-axis +2.5 degree" << endl;
-    cout << "f: Rotate Z-axis -2.5 degree" << endl; 
+    cout << "f: Rotate Z-axis -2.5 degree" << endl;
     cout << "m: Scale up +0.05" << endl;
     cout << "n: Scale down -0.05" << endl;
     cout << "up/down arrow: Move up/down" << endl;
     cout << "left/right arrow: Move left/right" << endl;
-    cout << "shift + up/down arrow: Move forward/backward" << endl;     
-    cout << "----------------------------------------" << endl;         
+    cout << "shift + up/down arrow: Move forward/backward" << endl;
+    cout << "----------------------------------------" << endl;
 
     glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(10, 10);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
     glutCreateWindow("2023000000202");
@@ -226,18 +233,14 @@ int main(int argc, char *argv[])
     glutSpecialFunc(specialKey);
     glutIdleFunc(idle);
 
-    glClearColor(0.4,0.4,0.4,1);
+    glClearColor(0.4, 0.4, 0.4, 1);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 
-    
-
     glutMainLoop();
 
     return EXIT_SUCCESS;
 }
-
-
